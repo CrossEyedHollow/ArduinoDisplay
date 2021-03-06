d58e8763b77d1215bb9cad1b1dae3994b86307b9
Lyubomir Lyubomirov <ayrona93@gmail.com>
2021-03-06 22:43:30 +0200

    Added Stopwatch class for ease of access of time

---
+++ graphicstest/Stopwatch.cpp
@@ -0,0 +1,47 @@
+#include "Arduino.h"
+class Stopwatch
+{
+private:
+    unsigned long startTime;
+    unsigned long stopTime;
+    bool stopped;
+    
+public:
+    Stopwatch() 
+    {
+        stopped = true;
+        startTime = 0;
+        stopTime = 0;
+    }
+
+    void Start()
+    {
+        if (stopped)
+        {
+            stopped = false;
+            startTime = millis();
+        }
+    }
+
+    void Stop()
+    {
+        stopped = true;
+        stopTime = millis();
+    }
+
+    void Restart()
+    {
+        startTime = millis();
+        stopTime = 0;
+        stopped = false;
+    }
+
+    unsigned long Elapsed()
+    {
+        return (stopped) ? (stopTime - startTime) : (millis() - startTime);
+    }
+};
+
+
+
+
